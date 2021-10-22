15e061f5fbd8ecba26424bd1ec5622101883ae11
linux <souhailelfad@gmail.com>
2021-10-22 21:09:30 +0200

    n

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE ="com.example.twoactivities.extra.MESSAGE";
     private int mCount = 0;
     private TextView mShowCount;
+    public static final int TEXT_REQUEST = 1;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
-        toast.show();
-    }
+
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
-
+    public void showToast(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mShowCount.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
 }
